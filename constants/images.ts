0b20773808c9715ffbcbf8b30a395c7355d5c037
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-09-14 16:03:25 +0300

    Preparation for publication in Google Play

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,19 @@
 import bg from "@/assets/images/bg.png";
+import categories from "@/assets/images/categories.png";
+import choice2 from "@/assets/images/choice-2.png";
+import choice from "@/assets/images/choice.png";
+import glasses from "@/assets/images/glasses.png";
 import highlight from "@/assets/images/highlight.png";
+import ingredients from "@/assets/images/ingredients.png";
 import rankingGradient from "@/assets/images/rankingGradient.png";
 
 export const images = {
     bg,
     highlight,
     rankingGradient,
+    categories,
+    ingredients,
+    glasses,
+    choice,
+    choice2,
 };
